1ec927ac87dd042d2ef663c49df566a55109673e
sumit <smitkawale@gmail.com>
2020-10-06 10:25:16 -0400

    Added cors

--- server.js
+++ server.js
@@ -2,19 +2,15 @@ const { EWOULDBLOCK } = require('constants');
 const express = require('express');
 const app = express();
 const port = 3000;
+const cors = require('cors');
 
-app.use('/', express.static('public'));
-
+app.use(cors());
 let budgetdata=require('./budgetdata.json');
-
-app.get('/hello', (req, res)=>{
-    res.send('Hello world');
-});
 
 app.get('/budget', (req, res)=>{
     res.json(budgetdata);
 });
 
 app.listen(port, () =>{
-    console.log(`Example app listening at http://localhost:${port}`);
+    console.log(`API served at http://localhost:${port}`);
 });
